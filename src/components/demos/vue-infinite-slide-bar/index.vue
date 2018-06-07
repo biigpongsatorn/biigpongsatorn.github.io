b1f38b26d5505ffe1d0a0b8ed180a1afac8c1089
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-06-07 12:17:05 +0700

    ✨ Add example of vue-infinite-slide-bar

--- src/components/demos/vue-infinite-slide-bar/index.vue
+++ src/components/demos/vue-infinite-slide-bar/index.vue
@@ -18,6 +18,27 @@
       {{ templateCode.props.html }}
       </highlight>
     </div>
+    <div class="my-sub-title pd-30px"># Cryptocurrencies</div>
+    <infinite-slide-bar>
+      <div class="columns">
+        <div class="column" v-for="coin in coins" :key="coin.key">
+          <div class="f-left pd-t-8px mg-r-10px">
+            <img :src="`/static/vue-infinite-slide-bar/crypto/${coin.key}.png`">
+          </div>
+          <div class="f-left">
+            <div class="f-w-bold">{{coin.name}}</div>
+            <div>{{coin.price}}
+              <span :class="{ 'cl-success': coin.change > 0, 'cl-danger': coin.change < 0 }">({{coin.change}}%)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </infinite-slide-bar>
+    <div class="f-left w-100pct pd-30px">
+      <highlight language="html" class="highlight">
+      {{ templateCode.crypto.html }}
+      </highlight>
+    </div>
   </div>
 </template>
 
@@ -36,8 +57,57 @@ export default {
           html: `<infinite-slide-bar duration="8s" direction="reverse" delay="5s" :barStyle="{ background: '#fff7cc', padding: '5px 0' }">
       <span style="color: #f60;">Your time is limited, so don't waste it living someone else's life.</span>
     </infinite-slide-bar>`
+        },
+        crypto: {
+          html: `<infinite-slide-bar>
+      <div class="columns">
+        <div class="column" v-for="coin in coins" :key="coin.key">
+          <div class="f-left pd-t-8px mg-r-10px">
+            <img :src="'/static/vue-infinite-slide-bar/crypto/' + coin.key + '.png'">
+          </div>
+          <div class="f-left">
+            <div class="f-w-bold">{{coin.name}}</div>
+            <div>{{coin.price}}
+              <span :class="{ 'cl-success': coin.change > 0, 'cl-danger': coin.change < 0 }">({{coin.change}}%)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </infinite-slide-bar>`
         }
-      }
+      },
+      coins: [
+        {
+          key: 'btc',
+          name: 'Bitcoin',
+          price: '$7,704',
+          change: 1.29
+        },
+        {
+          key: 'eth',
+          name: 'Ethereum',
+          price: '$612',
+          change: 1.04
+        },
+        {
+          key: 'xrp',
+          name: 'Ripple',
+          price: '$0.67',
+          change: -0.24
+        },
+        {
+          key: 'eos',
+          name: 'EOS',
+          price: '$13.91',
+          change: -0.83
+        },
+        {
+          key: 'ltc',
+          name: 'Litecoin',
+          price: '$122.31',
+          change: 1.07
+        }
+      ]
     }
   },
   components: { InfiniteSlideBar }
