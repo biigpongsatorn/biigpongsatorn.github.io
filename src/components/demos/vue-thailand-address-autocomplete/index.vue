d278fd572c54400c14dfcc5e30a8debd7b8c8ddb
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-06-20 00:40:35 +0700

    ✏️ Fix tag name

--- src/components/demos/vue-thailand-address-autocomplete/index.vue
+++ src/components/demos/vue-thailand-address-autocomplete/index.vue
@@ -2,10 +2,10 @@
   <div class="f-left w-100pct">
     <div class="f-left w-100pct bg-cl-light pd-30px bd-rd-5px">
       <div class="w-50pct-tablet">
-        <ThailandAutoComplete v-model="district" type="district" @select="select" label="ตำบล" size="small" placeholder="ตำบล..."/>
-        <ThailandAutoComplete v-model="amphoe" type="amphoe" @select="select" label="อำเภอ" color="#42b883" placeholder="อำเภอ..."/>
-        <ThailandAutoComplete v-model="province" type="province" @select="select" label="จังหวัด" size="medium" color="#35495e" placeholder="จังหวัด..."/>
-        <ThailandAutoComplete v-model="zipcode" type="zipcode" @select="select" label="รหัสไปรษณีย์" size="large" color="#00a4e4" placeholder="รหัสไปรษณีย์..."/>
+        <thailand-autocomplete v-model="district" type="district" @select="select" label="ตำบล" size="small" placeholder="ตำบล..."/>
+        <thailand-autocomplete v-model="amphoe" type="amphoe" @select="select" label="อำเภอ" color="#42b883" placeholder="อำเภอ..."/>
+        <thailand-autocomplete v-model="province" type="province" @select="select" label="จังหวัด" size="medium" color="#35495e" placeholder="จังหวัด..."/>
+        <thailand-autocomplete v-model="zipcode" type="zipcode" @select="select" label="รหัสไปรษณีย์" size="large" color="#00a4e4" placeholder="รหัสไปรษณีย์..."/>
       </div>
     </div>
     <div class="f-left w-100pct">
@@ -30,10 +30,10 @@ export default {
       templateCode: {
         html: `
     <template>
-      <ThailandAutoComplete v-model="district" type="district" @select="select" label="ตำบล" size="small" placeholder="ตำบล..."/>
-      <ThailandAutoComplete v-model="amphoe" type="amphoe" @select="select" label="อำเภอ" color="#42b883" placeholder="อำเภอ..."/>
-      <ThailandAutoComplete v-model="province" type="province" @select="select" label="จังหวัด" size="medium" color="#35495e" placeholder="จังหวัด..."/>
-      <ThailandAutoComplete v-model="zipcode" type="zipcode" @select="select" label="รหัสไปรษณีย์" size="large" color="#00a4e4" placeholder="รหัสไปรษณีย์..."/>
+      <thailand-autocomplete v-model="district" type="district" @select="select" label="ตำบล" size="small" placeholder="ตำบล..."/>
+      <thailand-autocomplete v-model="amphoe" type="amphoe" @select="select" label="อำเภอ" color="#42b883" placeholder="อำเภอ..."/>
+      <thailand-autocomplete v-model="province" type="province" @select="select" label="จังหวัด" size="medium" color="#35495e" placeholder="จังหวัด..."/>
+      <thailand-autocomplete v-model="zipcode" type="zipcode" @select="select" label="รหัสไปรษณีย์" size="large" color="#00a4e4" placeholder="รหัสไปรษณีย์..."/>
     </template>`,
         script: `
     export default {
