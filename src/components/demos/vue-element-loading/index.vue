d396a8bcbe318e6c250ec0d99fffda909c8ba7be
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-08-01 12:04:08 +0700

    complete example vue-element-loading

--- src/components/demos/vue-element-loading/index.vue
+++ src/components/demos/vue-element-loading/index.vue
@@ -1,116 +1,73 @@
 <template>
   <div class="f-left w-100pct">
-    <div class="my-sub-title pd-hrzt-30px"># Simple</div>
-    <infinite-slide-bar :barStyle="{ background: '#fff7cc', padding: '5px 0' }">
-      <span style="color: #f60;">Your time is limited, so don't waste it living someone else's life.</span>
-    </infinite-slide-bar>
+    <div class="my-sub-title pd-hrzt-30px"># Full Screen</div>
+    <div class="f-left w-100pct pd-hrzt-30px mg-vtc-10px">
+      <vue-element-loading :active="showFullScreen" :is-full-screen="true"/>
+      <a class="button" @click="startLoading('full')">
+        Start
+      </a>
+    </div>
     <div class="f-left w-100pct pd-hrzt-30px">
       <highlight language="html" class="highlight">
-      {{ templateCode.simple.html }}
+      {{ templateCode.fullscreen.html }}
       </highlight>
     </div>
-    <div class="my-sub-title pd-hrzt-30px"># Props usage</div>
-    <infinite-slide-bar :barStyle="{ background: '#fff7cc', padding: '5px 0' }" duration="8s" direction="reverse" delay="5s">
-      <span style="color: #f60;">Your time is limited, so don't waste it living someone else's life.</span>
-    </infinite-slide-bar>
-    <div class="f-left w-100pct pd-hrzt-30px">
-      <highlight language="html" class="highlight">
-      {{ templateCode.props.html }}
-      </highlight>
-    </div>
-    <div class="my-sub-title pd-30px"># Cryptocurrencies</div>
-    <infinite-slide-bar>
-      <div class="columns">
-        <div class="column" v-for="coin in coins" :key="coin.key">
-          <div class="f-left pd-t-8px mg-r-10px">
-            <img :src="`/static/vue-infinite-slide-bar/crypto/${coin.key}.png`">
-          </div>
-          <div class="f-left">
-            <div class="f-w-bold">{{coin.name}}</div>
-            <div>{{coin.price}}
-              <span :class="{ 'cl-success': coin.change > 0, 'cl-danger': coin.change < 0 }">({{coin.change}}%)</span>
-            </div>
-          </div>
-        </div>
+    <div class="my-sub-title pd-hrzt-30px"># Inside container</div>
+    <div class="f-left w-100pct pd-hrzt-30px mg-vtc-10px">
+      <div>
+        <a class="button" @click="startLoading('inside')">
+          Start
+        </a>
       </div>
-    </infinite-slide-bar>
-    <div class="f-left w-100pct pd-30px">
+      <div class="f-left pd-30px mg-vtc-10px bd-rd-5px bg-cl-light mx-w-200px">
+        <vue-element-loading :active="showInside" spinner="bar-fade-scale" color="#FF6700"/>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit.
+        </span>
+      </div>
+    </div>
+    <div class="f-left w-100pct pd-hrzt-30px">
       <highlight language="html" class="highlight">
-      {{ templateCode.crypto.html }}
+      {{ templateCode.inside.html }}
       </highlight>
     </div>
   </div>
 </template>
 
 <script>
-import InfiniteSlideBar from 'vue-infinite-slide-bar'
+import VueElementLoading from 'vue-element-loading'
 export default {
   data () {
     return {
       templateCode: {
-        simple: {
-          html: `<infinite-slide-bar :barStyle="{ background: '#fff7cc', padding: '5px 0' }">
-      <span style="color: #f60;">Your time is limited, so don't waste it living someone else's life.</span>
-    </infinite-slide-bar>`
+        fullscreen: {
+          html: `<vue-element-loading :active="isActive" :is-full-screen="true"/>`
         },
-        props: {
-          html: `<infinite-slide-bar duration="8s" direction="reverse" delay="5s" :barStyle="{ background: '#fff7cc', padding: '5px 0' }">
-      <span style="color: #f60;">Your time is limited, so don't waste it living someone else's life.</span>
-    </infinite-slide-bar>`
-        },
-        crypto: {
-          html: `<infinite-slide-bar>
-      <div class="columns">
-        <div class="column" v-for="coin in coins" :key="coin.key">
-          <div class="f-left pd-t-8px mg-r-10px">
-            <img :src="'/static/vue-infinite-slide-bar/crypto/' + coin.key + '.png'">
-          </div>
-          <div class="f-left">
-            <div class="f-w-bold">{{coin.name}}</div>
-            <div>
-              {{coin.price}}
-              <span :class="{ 'cl-success': coin.change > 0, 'cl-danger': coin.change < 0 }">({{coin.change}}%)</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </infinite-slide-bar>`
+        inside: {
+          html: `<div class="my-container">
+      <vue-element-loading :active="isActive" spinner="bar-fade-scale" color="#FF6700"/>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit.
+      </span>
+    </div>`
         }
       },
-      coins: [
-        {
-          key: 'btc',
-          name: 'Bitcoin',
-          price: '$7,704',
-          change: 1.29
-        },
-        {
-          key: 'eth',
-          name: 'Ethereum',
-          price: '$612',
-          change: 1.04
-        },
-        {
-          key: 'xrp',
-          name: 'Ripple',
-          price: '$0.67',
-          change: -0.24
-        },
-        {
-          key: 'eos',
-          name: 'EOS',
-          price: '$13.91',
-          change: -0.83
-        },
-        {
-          key: 'ltc',
-          name: 'Litecoin',
-          price: '$122.31',
-          change: 1.07
-        }
-      ]
+      showFullScreen: false,
+      showInside: false
     }
   },
-  components: { InfiniteSlideBar }
+  methods: {
+    startLoading (type) {
+      if (type === 'full') {
+        this.showFullScreen = true
+        setTimeout(() => { this.showFullScreen = false }, 3000)
+      } else {
+        this.showInside = true
+        setTimeout(() => { this.showInside = false }, 3000)
+      }
+    }
+  },
+  components: { VueElementLoading }
 }
 </script>
